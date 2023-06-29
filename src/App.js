7d920fce94799fbdcefca92eae6e20a7f5170abc
Xiaohong Xu <xxa52@sfu.ca>
2023-06-28 21:54:18 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React from'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: "OFF"
+    }
+  }
+
+  handleClick(){
+    this.setState({
+      status: this.state.status === "OFF" ? "ON" : "OFF"
+    })
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {this.state.status}
+          </p>
+          <button
+            className="App-link"
+            onClick = {this.handleClick.bind(this)}
+          >
+            CHANGE
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
